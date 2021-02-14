485a0ed7b2c3e31bc29886725b2bfba857a1f3fd
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-02-15 01:34:38 +0300

    Add files via upload

---
+++ TEST_HW1_TEST6/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int inputSec = Solution_6.readValue("Введите количество секунд, прошедших с начала суток: ");
+        int hours = Solution_6.calculateHours(inputSec);
+        int minutes = Solution_6.calculateMinutes(inputSec, hours);
+        int seconds = Solution_6.calculateSeconds(inputSec, hours, minutes);
+        Solution_6.printResult(hours , minutes, seconds);
+    }
+}
---
+++ TEST_HW1_TEST6/src/test/java/Solution_6Test.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_6Test {
+
+    @org.junit.jupiter.api.Test
+    void calculateHours() {
+        Solution_6 solution_6 = new Solution_6();
+        int expected = 1;
+        int actual = solution_6.calculateHours(3600);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateMinutes() {
+        Solution_6 solution_6 = new Solution_6();
+        int expected = 1;
+        int actual = solution_6.calculateMinutes(3660, 1);
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    void calculateSeconds() {
+        Solution_6 solution_6 = new Solution_6();
+        int expected = 1;
+        int actual = solution_6.calculateSeconds(3661, 1, 1);
+        assertEquals(expected, actual);
+    }
+}
