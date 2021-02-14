82173ec0283bfa75be4de0c1ba3ef96ce59e15e8
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-02-15 01:58:52 +0300

    Add files via upload

---
+++ TEST_HW1_TASK7/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        int x1 = Solution_7.readInt("Введите координаты первой точки (x1):");
+        int y1 = Solution_7.readInt("Введите координаты первой точки (y1):");
+        int x2 = Solution_7.readInt("Введите координаты второй точки (x2):");
+        int y2 = Solution_7.readInt("Введите координаты второй точки (y2):");
+        double interval1 = Solution_7.findInterval(x1, y1);
+        double interval2 = Solution_7.findInterval(x2, y2);
+        Solution_7.printResult(interval1, interval2);
+    }
+}
---
+++ TEST_HW1_TASK7/src/test/java/Solution_7Test.java
@@ -0,0 +1,16 @@
+import java.text.DecimalFormat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution_7Test {
+
+    @org.junit.jupiter.api.Test
+    void findInterval() {
+        Solution_7 solution_7 = new Solution_7();
+        DecimalFormat decimalFormat = new DecimalFormat("###.##");
+        double actualDouble = solution_7.findInterval(6, 7);
+        String actual = decimalFormat.format(actualDouble);
+        String expected = "9,22";
+        assertEquals(expected, actual);
+    }
+}
