60b5858bb9d1ee6422ae1338835f7ab94c6d8eda
Borisevichkat <52928737+Borisevichkat@users.noreply.github.com>
2021-02-17 23:51:15 +0300

    Add files via upload

---
+++ TEST_HW1_TASK10/src/main/java/epam/training/homework1/task10/Main.java
@@ -0,0 +1,14 @@
+package epam.training.homework1.task10;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int a = Solution_10.readInt("Введите точку а:");
+        int b = Solution_10.readInt("Введите точку b:");
+        int h = Solution_10.readInt("Введите h:");
+        HashMap <Integer, Double > table = Solution_10.createTable(a, b, h);
+        Solution_10.printTable(table);
+    }
+}
